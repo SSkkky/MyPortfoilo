81777a1819f7e5254a6ada6bd6088d4f98c30af1
SSkkky <worte5633@gmail.com>
2024-02-01 16:11:16 +0900

    icon font 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import '../styles/common.scss';
-import { ReactComponent as Logo } from '../assets/icons/cloud.svg';
+import { ReactComponent as Logo } from '../assets/icons/cloud-fill.svg';
 import { ReactComponent as WifiIcon } from '../assets/icons/wifi.svg';
 import { ReactComponent as BetteryIcon } from '../assets/icons/bettery.svg';
 import { ReactComponent as HanIcon } from '../assets/icons/han.svg';
-
+import dayjs from 'dayjs';
+import 'dayjs/locale/ko';
 function Home() {
+
+    dayjs.locale('ko')
+    let time = dayjs();
+
     return (
         <>
             <header>
                 <nav>
-                    <h1><Logo width="28" /></h1>
+                    <h1><Logo /></h1>
                     <ul>
                         <li>ABOUT_ME</li>
                         <li>PORTFOLIO</li>
@@ -23,7 +28,9 @@ function Home() {
                         100% <BetteryIcon width="28" stroke="#000" fill="#000" />
                     </div>
                     <HanIcon width="20" />
-                    <span>(금) 오후 12:10</span>
+                    <span>
+                        {time.format("(ddd) HH시 ss분")}
+                    </span>
                 </div>
             </header>
         </>
