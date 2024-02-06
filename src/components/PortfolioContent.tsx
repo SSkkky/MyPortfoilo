324531ac04a44cfc9b61297f8d0be93deb4782c9
SSkkky <worte5633@gmail.com>
2024-02-06 09:48:23 +0900

    axios json

--- src/components/PortfolioContent.tsx
+++ src/components/PortfolioContent.tsx
@@ -1,28 +1,27 @@
 import React, { useEffect, useState } from 'react'
-import { TitleIcon} from '../assets/icons/icons';
+import { TitleIcon } from '../assets/icons/icons';
 import axios from 'axios';
 
 type DataType = {
-    [key: string]: number | string | DataType[] | null;
-    id: number; 
-    name: string;
-    image_url: string;
+  [key: string]: number | string | DataType[] | null;
+  id: number;
+  name: string;
+  image_url: string;
 }
-  
+
 function PortfolioContent() {
-    const [loading, setLoading] = useState(false);
-    const [data, setData] = useState<DataType[] | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState<DataType[] | null>(null);
 
-    // useEffect(() => {
-    //     console.log(data, 'data');
-    // }, [data]);
-
-    // 데이터 로딩 함수
+  // 데이터 로딩 함수
   const fetchData = async () => {
     setLoading(true);
     try {
-      const response = await axios.get('../data.json');
-      setData(response.data);
+      await axios.get('./data.json')
+        .then(response => {
+          setData(response.data);
+          console.table(data);
+        });
     } catch (e) {
       console.error(e);
     }
@@ -31,34 +30,37 @@ function PortfolioContent() {
 
   useEffect(() => {
     fetchData();
+    console.table(data);
   }, []);
 
+  // 로딩 중일 때 로딩 화면 노출
   if (loading) {
-    return <div>Loading...!</div>  // 로딩 중일 때 로딩 화면 노출
+    return <div className='nowLoading'>로딩중입니다!</div>
   }
 
+  // 데이터가 없을 때
   if (!data) {
-    return null;  // 데이터가 없을 때
+    return <div className='nowError'>오류! 새로고침해주세요</div>;
   }
-    
+
 
   return (
     <div className='content portfolio'>
-        {
-            data?.map((item:DataType)=>
-                <section className='dot-bg portfolio-section' key={item.id}>
-                    <div className='dot-bg-inner'>
-                        <div className="content-bg-title">
-                            <TitleIcon />
-                            <p>{item.name}</p>
-                        </div>
-                        <div className={`content-bg-text pf-content-bg ${item.keyword}`}>
-                            <img src={item.image_url} alt="썸네일이미지"></img>
-                        </div>
-                    </div>
-                </section>
-            )
-        }
+      {
+        data?.map((item: DataType) =>
+          <section className='dot-bg portfolio-section' key={item.id}>
+            <div className='dot-bg-inner'>
+              <div className="content-bg-title">
+                <TitleIcon />
+                <p>{item.name}</p>
+              </div>
+              <div className={`content-bg-text pf-content-bg ${item.keyword}`}>
+                <img src={item.image_url} alt="썸네일이미지"></img>
+              </div>
+            </div>
+          </section>
+        )
+      }
     </div>
   )
 }
